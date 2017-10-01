77cd167de5c0a8808d3ade171e1189b11b675ad6
dallinder <dcallinder1s@gmail.com>
2017-09-30 21:52:22 -0600

    trying out testing with minitest in lesson 2

--- lesson2/car_test.rb
+++ lesson2/car_test.rb
@@ -1,18 +1,38 @@
 require 'minitest/autorun'
-require "minitest/reporters"
-Minitest::Reporters.use!
 
 require_relative 'car'
 
 class CarTest < MiniTest::Test
-  def test_wheels
-    car = Car.new
-    assert_equal(4, car.wheels)
+  def setup
+    @car = Car.new
   end
 
-  def test_bad_wheels
-    skip
-    car = Car.new
-    assert_equal(3, car.wheels)
+  def test_car_exists
+    assert(@car)
+  end
+
+  def test_wheels
+    assert_equal(4, @car.wheels)
+  end
+
+  def test_name_is_nil
+    assert_nil(@car.name)
+  end
+
+  def test_raise_initalize_with_arg
+    assert_raises(ArgumentError) do
+      car = Car.new(name: "Joey")
+    end
+  end
+
+  def assert_instance_of_car
+    assert_instance_of(Car, @car)
+  end
+
+  def test_includes_car
+    arr = [1, 2, 3]
+    arr << @car
+
+    assert_includes(arr, @car)
   end
 end
