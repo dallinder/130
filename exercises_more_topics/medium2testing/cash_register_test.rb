a38e79083dfe337aadebfa137324c76f22dbe95e
dallinder <dcallinder1s@gmail.com>
2017-10-02 20:34:30 -0600

    did first 6 exercises of medium testing

---
+++ exercises_more_topics/medium2testing/cash_register_test.rb
@@ -0,0 +1,43 @@
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+MiniTest::Reporters.use!
+
+require_relative 'cash_register'
+require_relative 'transaction'
+
+class CashRegisterTest < MiniTest::Test
+
+  def test_accept_money
+    register = CashRegister.new(1000)
+    transaction = Transaction.new(20)
+    transaction.amount_paid = 20
+
+    previous_amount = register.total_money
+    register.accept_money(transaction)
+    current_amount = register.total_money
+
+    assert_equal(previous_amount + 20, current_amount)
+  end
+
+  def test_change
+    register = CashRegister.new(1000)
+    transaction = Transaction.new(20)
+    transaction.amount_paid = 30
+
+    
+
+    assert_equal(10, register.change(transaction))
+  end
+
+  def test_give_receipt
+    register = CashRegister.new(1000)
+    transaction = Transaction.new(20)
+
+    output = <<~OUTPUT
+    You've paid $20.
+    OUTPUT
+
+    assert_output(output) { register.give_receipt(transaction) }
+  end
+end
